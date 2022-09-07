a55118afd13baa5e911a022a6a792aecc807fbc7
Pascal <pascal.allen88@gmail.com>
2022-09-06 21:15:22 -0500

    add GormUlid type to Role struct

--- src/Domain/Auth/Role/Role.go
+++ src/Domain/Auth/Role/Role.go
@@ -2,12 +2,13 @@ package Role
 
 import (
 	"github.com/oklog/ulid/v2"
+	"github.com/pascalallen/Baetyl/src/Adapter/Database/Type"
 	"github.com/pascalallen/Baetyl/src/Domain/Auth/Permission"
 	"time"
 )
 
 type Role struct {
-	Id          ulid.ULID               `json:"id" gorm:"primaryKey;size:26;not null"`
+	Id          Type.GormUlid           `json:"id" gorm:"primaryKey;size:26;not null"`
 	Name        string                  `json:"name"`
 	Permissions []Permission.Permission `json:"permissions,omitempty" gorm:"many2many:role_permissions"`
 	CreatedAt   time.Time               `json:"created_at"`
@@ -18,7 +19,7 @@ func Define(id ulid.ULID, name string) *Role {
 	createdAt := time.Now()
 
 	return &Role{
-		Id:         id,
+		Id:         Type.GormUlid(id),
 		Name:       name,
 		CreatedAt:  createdAt,
 		ModifiedAt: createdAt,
